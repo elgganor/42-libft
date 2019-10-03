dda43ab1a2d5f6454dc4a5f0875f99d18681d8ac
Mohamed-Younès Rouabehi <mohamed.rouabehi.pro@gmail.com>
2019-10-03 09:37:21 +0200

    Ajout de la fonction ft_isprint

--- ft_isprint.c
+++ ft_isprint.c
@@ -1,21 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isprint.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mrouabeh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/10/02 19:08:02 by mrouabeh          #+#    #+#             */
-/*   Updated: 2019/10/03 09:33:00 by mrouabeh         ###   ########.fr       */
+/*   Created: 2019/10/03 09:34:00 by mrouabeh          #+#    #+#             */
+/*   Updated: 2019/10/03 09:35:53 by mrouabeh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-#	define LIBFT_H
-
-int	ft_isdigit(int c);
-int	ft_isalpha(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
-#endif
+int	ft_isprint(int c)
+{
+	return (32 <= c && c < 127);
+}
