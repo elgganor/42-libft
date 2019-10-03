e4ac823b8b440b51b296ca41e1b2a20dee3b8536
Mohamed-Younès Rouabehi <mohamed.rouabehi.pro@gmail.com>
2019-10-03 10:07:33 +0200

    Ajout de la fonction ft_atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,27 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mrouabeh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/10/02 19:08:02 by mrouabeh          #+#    #+#             */
-/*   Updated: 2019/10/03 09:58:36 by mrouabeh         ###   ########.fr       */
+/*   Created: 2019/10/03 09:58:42 by mrouabeh          #+#    #+#             */
+/*   Updated: 2019/10/03 10:05:55 by mrouabeh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-#	define LIBFT_H
+#include "libft.h"
 
-#	include <string.h>
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	sign;
+	int	res;
 
-int	ft_isdigit(int c);
-int	ft_isalpha(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
-int	ft_toupper(int c);
-int	ft_tolower(int c);
-int	ft_atoi(const char *str);
-size_t	ft_strlen(const char *str);
-#endif
+	sign = 1;
+	i = 0;
+	if (str[i] == '-')
+	{
+		sign *= -1;
+		i++;
+	}
+	res = 0;
+	while (str[i] != '\0' && ft_isdigit(str[i]) == 1)
+	{
+		res = res * 10 + (str[i] - '0');
+		i++;
+	}
+	return (res * sign);
+}
