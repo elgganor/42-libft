f520cad965410610aea73ca7773918256e61be7c
Mohamed-Younès Rouabehi <mohamed.rouabehi.pro@gmail.com>
2019-10-03 10:22:21 +0200

    Ajout de la fonction ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,28 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mrouabeh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/10/02 19:08:02 by mrouabeh          #+#    #+#             */
-/*   Updated: 2019/10/03 10:17:08 by mrouabeh         ###   ########.fr       */
+/*   Created: 2019/10/03 10:17:21 by mrouabeh          #+#    #+#             */
+/*   Updated: 2019/10/03 10:20:18 by mrouabeh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-#	define LIBFT_H
+#include "libft.h"
 
-#	include <string.h>
+int	ft_strncmp(const char *s1, const char *s2)
+{
+	size_t	i;
 
-int	ft_isdigit(int c);
-int	ft_isalpha(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
-int	ft_toupper(int c);
-int	ft_tolower(int c);
-int	ft_atoi(const char *str);
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
-size_t	ft_strlen(const char *str);
-#endif
+	i = 0;
+	while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+}
