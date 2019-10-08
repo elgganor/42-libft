7a31a88d2cb1cb41e93e4bbae07d5ab4f6714b4a
mrouabeh <mrouabeh@student.42.fr>
2019-10-08 09:38:32 +0200

    Ajout de la fonction ft_memchr

---
+++ ft_memchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrouabeh <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/08 09:26:30 by mrouabeh          #+#    #+#             */
+/*   Updated: 2019/10/08 09:30:27 by mrouabeh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t			i;
+	unsigned char	*src;
+
+	src = (unsigned char *)s;
+	i = 0;
+	while (i < n)
+	{
+		if (src[i] == (unsigned char)c)
+			return (src + i);
+		i++;
+	}
+	return (0);
+}
