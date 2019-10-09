c21cffff3694f8a9125440a64ae77fb6ba37fd4e
mrouabeh <mrouabeh@student.42.fr>
2019-10-09 17:57:23 +0200

    Correction de ft_calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: mrouabeh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/08 09:54:36 by mrouabeh          #+#    #+#             */
-/*   Updated: 2019/10/08 10:08:14 by mrouabeh         ###   ########.fr       */
+/*   Updated: 2019/10/09 17:55:14 by mrouabeh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,13 @@
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*dest;
+	size_t	i;
 
+	i = 0;
 	if (count == 0 || size == 0)
 		return (0);
 	if (!(dest = (void *)malloc(size * count)))
 		return (0);
-	ft_memset(dest, 0, size * count);
+	ft_memset(dest, '\0', size * count);
 	return (dest);
 }
