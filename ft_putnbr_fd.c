ff21d72a221186a25fe216d55b8b8e06c530c601
mrouabeh <mrouabeh@student.42.fr>
2019-10-09 11:15:36 +0200

    Mise à la norme de ft_putnbr_fr

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: mrouabeh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/09 10:50:26 by mrouabeh          #+#    #+#             */
-/*   Updated: 2019/10/09 10:52:41 by mrouabeh         ###   ########.fr       */
+/*   Updated: 2019/10/09 11:15:03 by mrouabeh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,5 @@ void	ft_putnbr_fd(int n, int fd)
 	}
 	while (nb >= 10)
 		ft_putnbr_fd(nb / 10, fd);
-	ft_putchar_fd(nb % 10 + '0',fd);
+	ft_putchar_fd(nb % 10 + '0', fd);
 }
