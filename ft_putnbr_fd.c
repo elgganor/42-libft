f86a72899745b71aa9332a285b94d79847266f4b
mrouabeh <mrouabeh@student.42.fr>
2019-10-10 09:00:54 +0200

    Correction de ft_putnbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: mrouabeh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/09 10:50:26 by mrouabeh          #+#    #+#             */
-/*   Updated: 2019/10/09 18:07:34 by mrouabeh         ###   ########.fr       */
+/*   Updated: 2019/10/10 08:59:21 by mrouabeh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ void	ft_putnbr_fd(int n, int fd)
 	long	nb;
 
 	nb = n;
-	if (n < 0)
+	if (nb < 0)
 	{
 		ft_putchar_fd('-', fd);
 		nb *= -1;
 	}
 	if (nb >= 10)
 		ft_putnbr_fd(nb / 10, fd);
-	ft_putchar_fd(nb % 10 + '0', fd);
+	ft_putchar_fd((nb % 10) + '0', fd);
 }
