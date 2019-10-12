aedeea98080b42dd6d76a0126058e93c8c57e744
mrouabeh <mrouabeh@student.42.fr>
2019-10-12 16:58:07 +0200

    Début ft_lstnew

---
+++ ft_lstnew_bonus.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrouabeh <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/12 16:40:01 by mrouabeh          #+#    #+#             */
+/*   Updated: 2019/10/12 16:57:46 by mrouabeh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list *new;
+
+	new = (t_list *)malloc(sizeof(t_list));
+	new->next = NULL;
+	*(new->content) = *(content);
+   return (new);	
+}
