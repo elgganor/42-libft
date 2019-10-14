761acbeca2feb09d640f3f6e58bb021079be9a6a
mrouabeh <mrouabeh@student.42.fr>
2019-10-14 12:48:42 +0200

    Cast en unsigned char

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: mrouabeh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/08 09:02:32 by mrouabeh          #+#    #+#             */
-/*   Updated: 2019/10/09 11:22:02 by mrouabeh         ###   ########.fr       */
+/*   Updated: 2019/10/14 12:48:27 by mrouabeh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
-	size_t	i;
-	char	*dest;
-	char	*s;
+	size_t			i;
+	unsigned char	*dest;
+	unsigned char	*s;
 
-	s = (char *)src;
-	dest = (char *)dst;
+	s = (unsigned char *)src;
+	dest = (unsigned char *)dst;
 	i = 0;
 	while (i < n)
 	{
