444cb25f2bfb1eec9aacec0524cab320def4754d
mrouabeh <mrouabeh@student.42.fr>
2019-10-15 14:26:57 +0200

    memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: mrouabeh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/08 09:41:42 by mrouabeh          #+#    #+#             */
-/*   Updated: 2019/10/15 14:16:29 by mrouabeh         ###   ########.fr       */
+/*   Updated: 2019/10/15 14:26:49 by mrouabeh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 
 	s = (t_uchar *)src;
 	d = (t_uchar *)dst;
+	i = 0;
 	if (d > s)
 	{
 		while (0 < len)
