d8b243951b5bad0c8c3b24f8ef338e2b26a445a0
mrouabeh <mrouabeh@student.42.fr>
2019-10-15 15:21:40 +0200

    strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: mrouabeh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/04 10:23:21 by mrouabeh          #+#    #+#             */
-/*   Updated: 2019/10/15 15:01:16 by mrouabeh         ###   ########.fr       */
+/*   Updated: 2019/10/15 15:19:37 by mrouabeh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,10 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	len_ne = ft_strlen(needle);
 	if (needle[0] == '\0')
 		return ((char *)haystack);
-	while (haystack[i] != '\0')
+	while (haystack[i] != '\0' && i < len)
 	{
 		j = 0;
-		while (haystack[i + j] == needle[j] && j < len)
+		while (haystack[i + j] == needle[j] && (i + j) < len)
 		{
 			j++;
 			if (j == len_ne)
