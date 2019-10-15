6a7190509184e4acbbfd3968951b6d463c26c829
mrouabeh <mrouabeh@student.42.fr>
2019-10-15 18:43:16 +0200

    strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: mrouabeh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 14:32:52 by mrouabeh          #+#    #+#             */
-/*   Updated: 2019/10/07 15:03:53 by mrouabeh         ###   ########.fr       */
+/*   Updated: 2019/10/15 18:38:52 by mrouabeh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,19 +33,22 @@ char		*ft_strtrim(char const *s1, char const *set)
 	char	*sub;
 	char	*dest;
 
+	if (!s1)
+		return (NULL);
 	len = ft_strlen(s1);
 	i = 0;
 	while (ft_isset(s1[i], set) == 1)
 	{
 		i++;
 	}
+	if (s1[i] == '\0')
+		return ("");
 	sub = ft_substr(s1, i, len - i);
 	len = ft_strlen(sub);
-	i = len;
-	while (ft_isset(sub[i - 1], set) == 1)
+	while (ft_isset(sub[len - 1], set) == 1)
 	{
-		i--;
+		len--;
 	}
-	dest = ft_substr(sub, 0, i);
+	dest = ft_substr(sub, 0, len);
 	return (dest);
 }
