ba390ca314ca3d83f530ac598d3c7ecff7a8b8e1
mrouabeh <mrouabeh@student.42.fr>
2019-10-17 14:15:26 +0200

    Protection et norme

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: mrouabeh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/08 11:59:05 by mrouabeh          #+#    #+#             */
-/*   Updated: 2019/10/08 14:59:32 by mrouabeh         ###   ########.fr       */
+/*   Updated: 2019/10/17 14:15:14 by mrouabeh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,7 +56,7 @@ char			**ft_split(const char *s, char c)
 	int		j;
 
 	nb_word = ft_count_word(s, c);
-	if (!(res = (char **)malloc((nb_word + 1) * sizeof(char *))))
+	if (!s || !(res = (char **)malloc((nb_word + 1) * sizeof(char *))))
 		return (0);
 	i = 0;
 	j = 0;
