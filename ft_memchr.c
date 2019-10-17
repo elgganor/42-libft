d7e04f1b8f488019d1591c3a6af9e166424e178c
mrouabeh <mrouabeh@student.42.fr>
2019-10-17 12:43:11 +0200

    Protection de ft_memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: mrouabeh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/08 09:26:30 by mrouabeh          #+#    #+#             */
-/*   Updated: 2019/10/14 16:11:54 by mrouabeh         ###   ########.fr       */
+/*   Updated: 2019/10/17 12:42:59 by mrouabeh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	size_t			i;
 	unsigned char	*src;
 
+	if (!s)
+		return (0);
 	src = (unsigned char *)s;
 	i = 0;
 	while (i < n)
