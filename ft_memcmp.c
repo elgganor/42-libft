f0331bab6d2edf3b3d8b92b9e1c3423341b52789
mrouabeh <mrouabeh@student.42.fr>
2019-10-17 12:43:55 +0200

    Protection de memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: mrouabeh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/08 09:15:32 by mrouabeh          #+#    #+#             */
-/*   Updated: 2019/10/14 15:44:26 by mrouabeh         ###   ########.fr       */
+/*   Updated: 2019/10/17 12:43:45 by mrouabeh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,9 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	t_uchar	*src1;
 	t_uchar	*src2;
 	size_t	i;
-
+	
+	if (!s1 || !s2)
+		return (0);
 	src1 = (t_uchar *)s1;
 	src2 = (t_uchar *)s2;
 	i = 0;
