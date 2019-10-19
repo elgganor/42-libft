4d8f2feb07c883dcdcc45a583dc46399c25250cd
mrouabeh <mrouabeh@student.42.fr>
2019-10-19 14:08:05 +0200

    ft_memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: mrouabeh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 15:52:23 by mrouabeh          #+#    #+#             */
-/*   Updated: 2019/10/17 14:13:04 by mrouabeh         ###   ########.fr       */
+/*   Updated: 2019/10/19 14:07:48 by mrouabeh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 void	*ft_memset(void *b, int c, size_t len)
 {
-	char	*str;
-	size_t	i;
+	unsigned char	*str;
+	size_t			i;
 
 	if (!b)
 		return (NULL);
-	str = (char *)b;
+	str = (unsigned char *)b;
 	i = 0;
 	while (i < len)
 	{
