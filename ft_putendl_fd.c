c1c13976cb61e32029d9146b09f2eaee86aba74a
mrouabeh <mrouabeh@student.42.fr>
2019-10-21 12:35:23 +0200

    Protection

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -14,7 +14,10 @@
 
 void	ft_putendl_fd(char *s, int fd)
 {
-	while (*s)
-		ft_putchar_fd(*s++, fd);
-	ft_putchar_fd('\n', fd);
+	if (s)
+	{
+		while (*s)
+			ft_putchar_fd(*s++, fd);
+		ft_putchar_fd('\n', fd);
+	}
 }
