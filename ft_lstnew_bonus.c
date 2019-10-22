080af340caae146dde688b9e1831d5c28d004834
mrouabeh <mrouabeh@student.42.fr>
2019-10-22 12:48:56 +0200

    Modif

--- ft_lstnew_bonus.c
+++ ft_lstnew_bonus.c
@@ -15,22 +15,11 @@
 
 t_list	*ft_lstnew(void *content)
 {
-	size_t	data_size;
-	size_t	i;
 	t_list	*new_node;
 
-	if (!content)
-		return (NULL);
-	data_size = ft_strlen((char *)content);
 	if (!(new_node = (t_list *)malloc(sizeof(t_list))))
 		return (NULL);
+	new_node->content = content;
 	new_node->next = NULL;
-	new_node->content = malloc(data_size);
-	i = 0;
-	while (i < data_size)
-	{
-		*(char *)(new_node->content + i) = *(char *)(content + i);
-		i++;
-	}
 	return (new_node);
 }
--- partbonus.c
+++ partbonus.c
@@ -40,10 +40,7 @@ void affStr(void *data)
 }
 
 // Fonctions pour map, del et iter
-void	myDel(void *content)
-{
-	content = 0;
-}
+
 
 int	main(int ac, char **av)
 {
@@ -53,37 +50,7 @@ int	main(int ac, char **av)
 		if (strcmp(av[1], "ft_lstnew") == 0)
 		{
 			printf("==== ft_lstnew ====\n");
-			t_list *lstI;
-			int	i;
-
-			i = 42;
-			lstI = ft_lstnew(&i);
-			aff_list(lstI, &affInt);
-			free(lstI);
-
-			t_list *lstC;
-			char c;
-
-			c = 'c';
-			lstC = ft_lstnew(&c);
-			aff_list(lstC, &affChar);
-			free(lstC);
-
-			t_list *lstS;
-			char *str;
-
-			str = "Hello world";
-			lstS = ft_lstnew(str);
-			aff_list(lstS, &affStr);
-			free(lstS);
-
-			t_list *lstS2;
-			char *str2;
-
-			str2 = NULL;
-			lstS2 = ft_lstnew(str2);
-			aff_list(lstS2, &affStr);
-			free(lstS2);
+			
 		}
 
 
@@ -91,62 +58,21 @@ int	main(int ac, char **av)
 		if (strcmp(av[1], "ft_lstadd_front") == 0)
 		{
 			printf("==== ft_lstadd_front ====\n");
-			t_list *head;
-			t_list *elt1;
-			t_list *elt2;
-
-			int	i = 45;
-			int	j = 62;
-			int	k = 38;
-
-			head = ft_lstnew(&i);
-			elt1 = ft_lstnew(&j);
-			elt2 = ft_lstnew(&k);
-
-			head->next = elt1;
-			elt1->next = elt2;
-
-			int l = 55;
-			ft_lstadd_front(&head, ft_lstnew(&l));
-
-			aff_list(head, &affInt);
+			
 		}
 
 		// void	ft_lstadd_back(t_list **alst, t_list *new);
 		if (strcmp(av[1], "ft_lstadd_back") == 0)
 		{
 			printf("==== ft_lstadd_back ====\n");
-			t_list *head;
-			t_list *elt1;
-			t_list *elt2;
-
-			int i = 45;
-			int j = 62;
-			int k = 38;
-
-			head = ft_lstnew(&i);
-			elt1 = ft_lstnew(&j);
-			elt2 = ft_lstnew(&k);
-
-			head->next = elt1;
-			elt1->next = elt2;
-
-			int l = 55;
-			ft_lstadd_back(&head, ft_lstnew(&l));
-
-			aff_list(head, &affInt);
+			
 		}
 
 		// void	ft_lstdelone(t_list *lst, void (*del)(void *));
 		if (strcmp(av[1], "ft_lstdelone") == 0)
 		{
 			printf("==== ft_lstdelone ====\n");
-			t_list *head;
-			char *str = "hello";
-			head = ft_lstnew(str);
-
-			ft_lstdelone(head, &myDel);
-			aff_list(head, &affStr);
+			
 		}
 
 
