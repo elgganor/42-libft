99fc6826ad253d3fc83d54d7e4a89576e7557af0
mrouabeh <mrouabeh@student.42.fr>
2019-10-22 17:57:22 +0200

    Ajout des bonus

--- libft.h
+++ libft.h
@@ -62,4 +62,13 @@ void			*ft_memchr(const void *s, int c, size_t n);
 void			*ft_memmove(void *dst, const void *src, size_t len);
 void			*ft_memset(void *b, int c, size_t len);
 void			*ft_calloc(size_t count, size_t size);
+int				ft_lstsize(t_list *lst);
+void			ft_lstadd_front(t_list **alst, t_list *new);
+void			ft_lstadd_back(t_list **alst, t_list *new);
+void			ft_lstdelone(t_list *lst, void (*del)(void *));
+void			ft_lstclear(t_list **lst, void (*del)(void *));
+void			ft_lstiter(t_list *lst, void (*f)(void *));
+t_list			*ft_lstnew(void *content);
+t_list			*ft_lstlast(t_list *lst);
+t_list			*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 #endif
